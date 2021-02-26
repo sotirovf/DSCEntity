929e23372b02a583b34fcf1faad0d61577cddbab
FS Coding <sotirovf@gmail.com>
2021-02-26 13:48:21 +0000

    add-migrations and populating the created DB with values

---
+++ Migrations/202102260451174_FillDatabase.cs
@@ -0,0 +1,21 @@
+﻿namespace DSCEntity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FillDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO dbo.ItemModels(Name, Type, Size, Price, Description) VALUES('Jeans', 'M', '35', '15.00', 'Description to come') ");
+            Sql("INSERT INTO dbo.ItemModels(Name, Type, Size, Price, Description) VALUES('T-Shirt', 'W', '12', '8.00', 'Description to come') ");
+            Sql("INSERT INTO dbo.ItemModels(Name, Type, Size, Price, Description) VALUES('Jacket', 'W', '15', '99.99', 'Description to come') ");
+            Sql("INSERT INTO dbo.ItemModels(Name, Type, Size, Price, Description) VALUES('Boots', 'W', '8', '36.00', 'Description to come') ");
+            Sql("INSERT INTO dbo.ItemModels(Name, Type, Size, Price, Description) VALUES('Boots', 'M', '10', '55.00', 'Description to come') ");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
